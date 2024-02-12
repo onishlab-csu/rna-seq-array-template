28a4b05642228fe33f5eecad27499d81b6b862b9
David <davidcking.inbox@gmail.com>
2024-02-11 23:46:26 -0700

    updated check-settings.sh

--- check-settings.sh
+++ check-settings.sh
@@ -2,41 +2,76 @@
 SETTINGS=project-settings.bashrc
 source $SETTINGS
 
-if [ -n "$PROJ_ROOT" ]
+green() {
+    echo "\\033[0;32m$@\\033[0m"
+}
+orange() {
+    echo "\\033[0;33m$@\\033[0m"
+}
+red() {
+    echo "\\033[0;31m$@\\033[0m"
+}
+purple() {
+    echo "\\033[0;35m$@\\033[0m"
+}
+gray() {
+    echo "\\033[1;37m$@\\033[0m"
+}
+
+bold() {
+    echo "\\033[1m$@\033[0m"
+}
+
+highlight_bold() {
+    whole_str=$1
+    highlight_str=$2
+    echo ${whole_str/$highlight_str/$(bold $highlight_str)}
+}
+
+highlight_base() {
+    echo $(highlight_bold $1 $(basename $1))
+}
+
+if [ -z "$PROJ_ROOT" ] # PROJ_ROOT is missing/not set
 then
-    if [ -e "$PROJ_ROOT" ]
-    then # PROJ_ROOT is set correctly
-        echo "yay PROJ_ROOT=$PROJ_ROOT"
+    echo "No value for PROJ_ROOT is set."
+    echo "You should set it to the current directory by placing the following line at the top of $SETTINGS:"
+    echo "PROJ_ROOT=\"$PWD\""
+    exit 1
+
+else # see if setting is an existing directory
+
+    echo -ne "$(orange "checking\tPROJ_ROOT")\t$PROJ_ROOT: "
+    if ! [ -e "$PROJ_ROOT" ]
+    then # PROJ_ROOT is not there
+        echo -e $(red no such directory.)
+        echo "PROJ_ROOT is set to \"$PROJ_ROOT\" but it does not exist."
+        echo "Check your settings in $SETTINGS and rerun this script."
+        exit 1
+    else # PROJ_ROOT is set correctly
+        echo -e $(green exists.)
 
         # INPUTDIR  
-        echo -n "checking INPUTDIR \"$INPUTDIR\": "
+        echo -ne "$(orange "checking\tINPUTDIR")\t$(highlight_base $INPUTDIR): "
         if [ -d "$INPUTDIR" ]
         then 
-            echo "exists."
+            echo -e $(green exists.)
         else
-            echo "no such directory."
-            echo "create it with \`mkdir -p \"$INPUTDIR\"\`"
+            echo -e $(red no such directory.)
+            echo -e "$(purple Check your setting for INPUTDIR or create with \`mkdir -p \"$INPUTDIR\"\`)"
             exit 1
         fi
 
         # HISAT2PATH
-        echo -n "checking HISAT2PATH \"$HISAT2PATH\": "
+        echo -ne "$(orange "checking\tHISAT2PATH")\t$(highlight_base $HISAT2PATH): "
         if [ -d "$HISAT2PATH" ]
         then
-            echo "exists."
+            echo -e $(green exists.)
         else
-            echo "no such directory."
-            echo "Check your setting for HISAT2PATH, or create with \`mkdir -p \"$HISAT2PATH\"\`"
+            echo -e $(red no such directory.)
+            echo -e "$(purple Check your setting for HISAT2PATH or create with \`mkdir -p \"$HISAT2PATH\"\`)"
             exit 1
         fi
         
-        
-    else # PROJ_ROOT is not there
-        echo "PROJ_ROOT is set to \"$PROJ_ROOT\" but does not exist."
-        echo "Check your settings in $SETTINGS and rerun this script."
     fi
-else # PROJ_ROOT is missing
-    echo "PROJ_ROOT is not set."
-    echo "You should set it to the current directory by placing the following line at the top of $SETTINGS:"
-    echo "PROJ_ROOT=\"$PWD\""
 fi
